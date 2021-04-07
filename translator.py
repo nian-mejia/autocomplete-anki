99ee4876576fe0bac329463bd59dfda4a2e5c4e3
Nicoll Angulo <nianmejia16@gmail.com>
2021-04-06 21:29:21 -0500

    UPDATE

--- translator.py
+++ translator.py
@@ -1,46 +1,8 @@
-from googletrans import Translator
 from deep_translator import GoogleTranslator
 
-
-def solicitud(word):
-    word = word.lower()
-
-    translator = Translator()
-    language = translator.detect(word)
-    language = language.lang
-
-    if type(language) == list:
-        if language[0] == "en" or language[0] == "es":
-            return word, language[0]
-        else:
-            solicitud(word)
-    else:
-        if language == "en" or language == "es":
-            return word, language
-        else:
-            solicitud(word)
-
-
-def do_translat(translator, word):
-    try:
-        word, language = solicitud(word)
-    except TypeError:
-        word, language = solicitud(word)
-
-    if language == "en":
-        dest = "es"
-    else:
-        dest = "en"
-    
-    try:
-        translated = translator(source=language, target=dest).translate(word)
-        return translated
-    except:
-        print("error")
-        return "error"
-
 def googletrans(word):
-    translated = do_translat(GoogleTranslator, word)
-    print(translated.capitalize())
-    return translated.capitalize()
+    translater = GoogleTranslator(source="en", target="es")
+    word_trans = translater.translate(word)
+    print(word_trans)
+    return word_trans
 
